c17bcea0e8fdaf927b084f02575f47ab32f92e09
darvin pasigna <darvinpasigna@gmail.com>
2024-02-20 18:26:09 +0800

    4. assembling timers and addtimer with useTimerCOntext

--- src/components/AddTimer.tsx
+++ src/components/AddTimer.tsx
@@ -2,14 +2,20 @@ import { useRef } from 'react';
 import Button from './UI/Button.tsx';
 import Form, { FormHandle } from './UI/Form.tsx';
 import Input from './UI/Input.tsx';
+import { useTimersContext } from '../Store/timers-context.tsx';
 
 // previous App code transferred to this component.
 const AddTimer = () => {
   const form = useRef<FormHandle>(null);
+//access useTImerContext to be able add timer
+const {addTimer} = useTimersContext();
 
   function handleSaveTimer(data: unknown) {
     const extractedData = data as { name: string; duration: string };
-    console.log(extractedData);
+    addTimer({
+      name: extractedData.name,
+      duration: +extractedData.duration, // + type casts to number
+    });
     form.current?.clear();
   }
 
--- src/components/Timers.tsx
+++ src/components/Timers.tsx
@@ -1,4 +1,17 @@
+import { useTimersContext } from "../Store/timers-context.tsx";
+import Timer from "./Timer.tsx";
+
 const Timers = () => {
-  return <ul>{/* TODO ... */}</ul>;
+  const {timers} = useTimersContext();
+  return (
+    //list of timers
+    <ul>
+      {timers.map((timer)=>(
+        <li key={timer.name}>
+          <Timer {...timer} /></li>
+      ))}
+      
+    </ul>
+  );
 }
 export default Timers;
